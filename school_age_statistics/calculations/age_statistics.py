b0deb5fc6145412f39a1c8a3ebb87996e7298fb0
sdelgadoro <sdelgadoro@ucenfotec.ac.cr>
2026-07-05 22:46:21 -0600

    Update age_statistics.py

--- school_age_statistics/calculations/age_statistics.py
+++ school_age_statistics/calculations/age_statistics.py
@@ -1,24 +1,24 @@
 def calculo_edad_máxima (muestra_edades):
-  return max(muestra_edades["edades"])
+  return max(muestra_edades["ages"])
 
 def calculo_edad_minima (muestra_edades):
-  return min(muestra_edades["edades"])
+  return min(muestra_edades["ages"])
 
 def calculo_promedio_edades (muestra_edades):
-  edades = muestra_edades ["edades"]
+  edades = muestra_edades ["ages"]
   promedio = sum(edades)/ len(edades)
   return round(promedio, 1)
 
 def calculo_mediana_edades (muestra_edades):
-  edades = sorted (muestra_edades["edades"])
+  edades = sorted (muestra_edades["ages"])
 
-total = len (edades)
+total = len(edades)
 centro = total // 2
 
 if total % 2 == 1:
   return edades[centro]
 
-return (edades[centro] - 1 + edades [centro]) / 2
+return (edades[centro -1] + edades [centro]) / 2
 
 def calcular_estadisticas(muestra_edades): #Para respetar como esta hecho en Main.
   return {
