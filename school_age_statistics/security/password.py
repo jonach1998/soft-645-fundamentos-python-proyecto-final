c83c64513d6f906888495fa3d924ae39634f48d6
Jonathan Chavarria <jonach1998@gmail.com>
2026-07-07 09:49:14 -0600

    Refactor password validation to use getpass for secure input and clean up whitespace

--- school_age_statistics/security/password.py
+++ school_age_statistics/security/password.py
@@ -1,18 +1,19 @@
 import getpass
 
+
 def validar_contrasena():
     contrasena_correcta = "SanPascualin2026"
     intentos = 3
-    
+
     while intentos > 0:
-        ingreso = input("LOGIN: Ingrese la contraseña: ")
-        
+        ingreso = getpass.getpass("LOGIN: Ingrese la contraseña: ")
+
         if ingreso == contrasena_correcta:
             print("Acceso concedido con éxito\n")
             return True
         else:
             intentos -= 1
             print(f"Contraseña incorrecta. Intentos restantes: {intentos}\n")
-            
+
     print("Acceso denegado. Demasiados intentos fallidos.")
     return False
